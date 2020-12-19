9f138c49d77b9b1a0219aee1afd0dbadc7cff5d0
Sullyvan Puppi <spuppi.gft@gmail.com>
2020-12-19 17:03:09 -0300

    multicloud init

---
+++ multicloud-function/src/main/java/com/santander/arsenal/serverless/multicloudfunction/MulticloudFunctionApplication.java
@@ -0,0 +1,13 @@
+package com.santander.arsenal.serverless.multicloudfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulticloudFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulticloudFunctionApplication.class, args);
+	}
+
+}
---
+++ multicloud-function/src/test/java/com/santander/arsenal/serverless/multicloudfunction/MulticloudFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.arsenal.serverless.multicloudfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulticloudFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
